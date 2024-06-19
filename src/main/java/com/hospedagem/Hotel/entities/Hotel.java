8e39ae9d608c94d18aad79c1ddcc48534bc24e4e
Thiago-Souzaa <tssti@outlook.com>
2024-06-18 22:46:42 -0300

    ok

--- src/main/java/com/hospedagem/Hotel/entities/Hotel.java
+++ src/main/java/com/hospedagem/Hotel/entities/Hotel.java
@@ -2,11 +2,21 @@ package com.hospedagem.Hotel.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_hotel")
 public class Hotel implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String localizacao;
